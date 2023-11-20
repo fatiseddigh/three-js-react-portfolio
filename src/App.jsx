73a868c83ced433679344db6ba67c3a711d65c97
fatiseddigh <seddighfati@gmail.com>
2023-11-20 16:50:57 +0330

    feat:create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-function App() {
+import Intro from "./components/Intro";
+import Who from "./components/Who";
+import Works from "./components/Works";
+import Contacts from "./components/Contacts";
 
+function App() {
   return (
     <div>
-      Hello world!
+      <Intro />
+      <Who />
+      <Works />
+      <Contacts />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
