1873c0a1e73a82425a0654f6efa634f4ab754ecb
fatiseddigh <seddighfati@gmail.com>
2023-11-21 16:08:52 +0330

    feat:add text of works components

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,11 +1,61 @@
 import React from "react";
 import styled from "styled-components";
+const data = [
+  "Web Design",
+  "Development",
+  "Illustration",
+  "Product Design",
+  "Social Media",
+];
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Container = styled.div`
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+const List = styled.div`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+const ListItem = styled.div`
+  font-size: 75px;
+  font-weight: bold;
+  color: transparent;
+  -webkit-text-stroke: 1px #ffccff;
+`;
+const Right = styled.div`
+  flex: 1;
 `;
 const Works = () => {
-  return <Section>Works</Section>;
+  return (
+    <Section>
+      <Container>
+        <Left>
+          <List>
+            {data.map((item) => (
+              <ListItem key={item} text={item} onClick={() => setWork(item)}>
+                {item}
+              </ListItem>
+            ))}
+          </List>
+        </Left>
+        <Right></Right>
+      </Container>
+    </Section>
+  );
 };
 
 export default Works;
