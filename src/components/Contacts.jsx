22ca12249e3bc12cc4c2fd8f91c549bce5821cb3
fatiseddigh <seddighfati@gmail.com>
2023-11-21 17:32:17 +0330

    feat:add contact form

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -4,8 +4,83 @@ const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
 `;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 10px;
+`;
+
+const Left = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  width: 420px;
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Input = styled.input`
+  padding: 20px;
+  background-color: #e8e6e6;
+  border: none;
+  border-radius: 5px;
+`;
+
+const Textarea = styled.textarea`
+  padding: 20px;
+  background-color: #e8e6e6;
+  border: none;
+  border-radius: 5px;
+`;
+
+const Button = styled.button`
+  background-color: #ff00ff;
+  color: white;
+  border: none;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 5px;
+  padding: 20px;
+`;
+
+const Right = styled.div`
+  display: flex;
+  flex: 1;
+`;
+
 const Contacts = () => {
-  return <Section>Contacts</Section>;
+  return (
+    <Section>
+      <Container>
+        <Left>
+          <Form>
+            <Title>Contact Us</Title>
+            <Input placeholder="Name" name="name" />
+            <Input placeholder="Email" name="email" />
+            <Textarea
+              placeholder="Write your message"
+              name="message"
+              rows={10}
+            />
+            <Button type="submit">Send</Button>
+          </Form>
+        </Left>
+        <Right></Right>
+      </Container>
+    </Section>
+  );
 };
 
 export default Contacts;
