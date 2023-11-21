7d69cfeaaa700626d15e9018f4f8a2a3a9d46201
fatiseddigh <seddighfati@gmail.com>
2023-11-21 13:11:58 +0330

    feat:add navbar

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import styled from "styled-components";
+import Navbar from "./Navbar";
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: start;
 `;
 const Intro = () => {
-  return <Section>Intro</Section>;
+  return (
+    <Section>
+      <Navbar />
+      fdfgd
+    </Section>
+  );
 };
 
 export default Intro;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import styled from "styled-components";
+const Section = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const Container = styled.div`
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 0px;
+`;
+const Links = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+const Logo = styled.img`
+  height: 45px;
+  width: 90px;
+`;
+const List = styled.ul`
+  display: flex;
+  list-style: none;
+  gap: 20px;
+`;
+const ListItem = styled.li`
+  cursor: pointer;
+`;
+const Icons = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+const Icon = styled.img`
+  width: 20px;
+  cursor: pointer;
+`;
+const Button = styled.button`
+  width: 100px;
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  color: white;
+  background-color: #ff00ff;
+  font-weight: bold;
+  cursor: pointer;
+`;
+const Navbar = () => {
+  return (
+    <Section>
+      <Container>
+        <Links>
+          <Logo src="./img/logos.svg" />
+          <List>
+            <ListItem>Home</ListItem>
+            <ListItem>Studio</ListItem>
+            <ListItem>Works</ListItem>
+            <ListItem>Contact</ListItem>
+          </List>
+        </Links>
+        <Icons>
+          <Icon src="./img/search.png" />
+          <Button>Hire Now</Button>
+        </Icons>
+      </Container>
+    </Section>
+  );
+};
+
+export default Navbar;
