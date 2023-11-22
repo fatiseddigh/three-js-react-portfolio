399ba81665f1d0ac1da60d36078b5fac799f2dd9
fatiseddigh <seddighfati@gmail.com>
2023-11-22 15:21:06 +0330

    feat:add cube animation text

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useRef } from "react";
 import { PerspectiveCamera, RenderTexture, Text } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 const Cube = () => {
+  const textRef = useRef();
+  useFrame(
+    (state) =>
+      (textRef.current.position.x = Math.sin(state.clock.elapsedTime) * 2)
+  );
   return (
     <mesh>
-      <boxGeometry args={[2, 2, 2]} />
+      <boxGeometry />
       <meshStandardMaterial>
         <RenderTexture attach="map">
-          <color attach="background" args={["pink"]} />
-          <PerspectiveCamera makeDefault position={[0, 0, 2]} />
-          <Text fontSize={1} color="#555">
+          <color attach="background" args={["#ff99ff"]} />
+          <PerspectiveCamera makeDefault position={[0, 0, 5]} />
+          <Text ref={textRef} fontSize={1.5} color="#555">
             SEDDIGH
           </Text>
         </RenderTexture>
